247b0c7e2f172b9301f93debac154c42c8e70b37
Singee <imsingee@gmail.com>
2017-10-21 20:56:54 +0800

    Bug Fixed

--- webapp/forms.py
+++ webapp/forms.py
@@ -62,7 +62,7 @@ class PostForm(Form):
 
     text = TextAreaField(u'内容', [DataRequired()])
 
-    is_published = BooleanField(u"发布？（不勾选只是存为草稿", default=False)
+    is_published = BooleanField(u"发布？（不勾选只是存为草稿）", default=False)
 
 
 class LoginForm(Form):
